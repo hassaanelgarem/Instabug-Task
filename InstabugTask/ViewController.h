e65cdc93e85936e8ecc700100c9a7759c1a52152
Hassaan El-Garem <hassaanelgarem@eseed.net>
2018-07-04 13:28:59 +0200

    first commit

---
+++ InstabugTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstabugTask
+//
+//  Created by Hassaan El-Garem on 7/4/18.
+//  Copyright © 2018 Garem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InstabugTask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InstabugTask
+//
+//  Created by Hassaan El-Garem on 7/4/18.
+//  Copyright © 2018 Garem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
